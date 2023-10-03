236c335e447b2e0b370dd35b8e5dc6621f661801
itrabbi <hassan.rabbi6321@gmail.com>
2023-10-03 22:50:36 +0600

    Initial commit

---
+++ swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: ".my_next",
+        prevEl: ".my_prev",
+    },
+  });
